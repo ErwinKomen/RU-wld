a0ddea76b59e5f3833845e9c6731e1b4608d41d3
ErwinKomen <E.Komen@ru.nl>
2017-01-26 16:52:21 +0100

    Allow for google analytics

--- wld/wld/dictionary/static/dictionary/scripts/diadict.js
+++ wld/wld/dictionary/static/dictionary/scripts/diadict.js
@@ -8,6 +8,18 @@ $(document).ready(function () {
 
 var oProgressTimer = null;
 
+// GOOGLE TRACKING STATISTICS
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-90924826-1', 'auto');
+ga('send', 'pageview');
+// ==================================
+
 /**
  * Goal: initiate a lemma search
  * Source: http://www.javascript-coder.com/javascript-form/javascript-reset-form.phtml
